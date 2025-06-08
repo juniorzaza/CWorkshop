0f5bf3d034e96085c045ffdae37edf8047a0687c
juniorzaza <chnolasit2546@gmail.com>
2025-06-08 10:43:01 +0700

    v.1 สร้ร้างไฟล์ 6 ไฟล์ เขียนโฟรแกรมไปนิดหน่อย

---
+++ lecture05.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+void main(){
+    printf("Hello....\n");
+    printf("Hi....\n");
+    printf("Hey....\n");
+    printf("Wow\t....");
+    printf("\t\t\tWoo....");
+    printf("Wee....\n");
+    printf("I\no\nT\nS\nA\nU");
+}

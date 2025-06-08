d7d9e161f79581c2b9674316c445becb9b874c2e
juniorzaza <chnolasit2546@gmail.com>
2025-06-08 11:51:17 +0700

    v3 แก 07 แล้วก้ทดสอบ้

---
+++ lecture07.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main(){
+    int aa = 10;
+    float bb = 20.1245;
+    double cc = 30.562165;
+    char dd = '#';
+    char ee[] = "hello";
+    long ff = 55646546;
+
+    printf("aa : %d", aa );
+    printf("\nbb : %f", bb );
+    printf("\ncc : %lf", cc );
+    printf("\ndd : %c", dd );
+    printf("\nee : %s", ee );
+    printf("\nff : %ld", ff );
+}
